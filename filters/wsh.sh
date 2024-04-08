9d9ecaeeea006157843a5d08f95bb103788c4968
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2024-04-08 16:41:43 -0600

    refactor prompt-setting

--- filters/wsh.sh
+++ filters/wsh.sh
@@ -5,28 +5,16 @@
 #   Note that setting and dereferencing variables works,
 #   and has no effect on the parent shell from which it was forked.
 
-setup() {
+set_prompt() {
         # set prompts to those used by the 1971 Thompson shell
-        if [ $(id -u) = 0 ]; then
-                PS1='# '  # original root prompt
-        else
-                PS1='@ '  # default, non-root prompt
-        fi
+        if test $(id -u) = 0; then PS1='# '; else PS1='@ '; fi
 }
 repl() {
     # read-execute-print loop
     while read -p "$PS1" line
     do
-            process_line "$line"
+        eval "$line"  # analogous to calls to "execve()" in bash, zsh, et al.
     done
 }
-process_line() {
-        eval "$*"  # analogous to calls to "execve()" in bash, zsh, et al.
-}
-shutdown() {
-        echo "logging out" # executed on ^D
-}
-
-setup
+set_prompt
 repl
-shutdown
